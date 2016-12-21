57325c29cd9d475cd1ee97b8a568fab0b8df2553
won <wjin0352@gmail.com>
2016-12-20 21:59:06 -0500

    added jquery scrolling effect

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 $( document ).ready(function() {
+    console.log('ready')
   // this line just initializes all popover messages, for detail refer to bootstrap popovers
   $('[data-toggle="popover"]').popover();
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
 });
